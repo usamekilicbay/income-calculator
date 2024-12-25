61a10fdbf925867869a3013a1d53792f8ba9ab41
Üsame <usame.kilicbay@gmail.com>
2024-12-25 12:28:45 +0300

    create: links component

---
+++ src/components/Links.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { FaGithub, FaLinkedin } from "vue3-icons/fa";
+</script>
+
+<template>
+  <div class="flex space-x-2">
+    <a href="https://www.linkedin.com/in/usame-kilicbay/"> <FaLinkedin /> </a>
+    <a href="https://github.com/usamekilicbay/"><FaGithub /> </a>
+  </div>
+</template>
+
+<style lang="css" scoped>
+a {
+  color: orange;
+  font-size: larger;
+}
+</style>
