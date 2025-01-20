6f5f787ee03971513e09206069283648f3be55c2
Üsame <usame.kilicbay@gmail.com>
2025-01-21 01:17:09 +0300

    create: type.ts and move all types here

---
+++ src/common/type.ts
@@ -0,0 +1,22 @@
+export type TColumns = {
+  hourly: boolean;
+  daily: boolean;
+  weekly: boolean;
+  montly: boolean;
+  annually: boolean;
+};
+
+export type TFormula = {
+  hours: number;
+  days: number;
+  annualLeave: number;
+};
+
+export type TMoney = {
+  amount: number;
+} & TCurrency;
+
+export type TCurrency = {
+  name: string;
+  abbr: string;
+};
