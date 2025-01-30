0065e71256be20fa6842ccd1f73b2139ba8e4299
Üsame <usame.kilicbay@gmail.com>
2025-01-30 14:40:44 +0300

    update!: major changes on CalculatorMain mainly depending on child components

--- src/components/CalculatorMain.vue
+++ src/components/CalculatorMain.vue
@@ -2,22 +2,30 @@
 import axios from "axios";
 import CurrencyRow from "./CurrencyRow.vue";
 import CurrencySelector from "./CurrencySelector.vue";
-import { onMounted, ref } from "vue";
-import useLocalStorage from "../helpers/useLocalStorage";
-import Links from "./Links.vue";
+import { onMounted, provide, Reactive, ref } from "vue";
+import useLocalStorageRef from "../helpers/useLocalStorageRef";
+import Navbar from "./Navbar.vue";
+import Work from "./Formula.vue";
+import CurrencyHeader from "./CurrencyHeader.vue";
+import useLocalStorageReactive from "../helpers/useLocalStorageReactive";
+import { TColumns, TCurrency, TFormula, TMoney } from "../common/type";
 
-export type TMoney = {
-  amount: number;
-} & TCurrency;
-
-export type TCurrency = {
-  name: string;
-  abbr: string;
-};
-
+const columns = useLocalStorageReactive<TColumns>(
+  {
+    hourly: true,
+    daily: true,
+    weekly: true,
+    montly: true,
+    annually: true,
+  },
+  "columns"
+);
+const formula = ref<TFormula>();
 const activeCurrency = ref<TCurrency>();
-const currencies = useLocalStorage<TCurrency[]>([], "currencies");
+const currencies = useLocalStorageRef<TCurrency[]>([], "currencies");
 const exchangeRates = ref<{ activeCurrency: TCurrency; rates: TMoney[] }>();
+
+provide<Reactive<TColumns>>("columns", columns);
 
 const prepare = async () => {
   if (!currencies || currencies.value.length === 0) {
@@ -68,15 +76,14 @@ const addNewCurrency = async (currency: TCurrency) => {
   }
 
   currencies.value.push(currency);
-
   await updateActiveCurrency(currency);
 };
 
-const removeCurrency = (currency: TCurrency) => {
+const removeCurrency = (currency: TCurrency): void => {
   currencies.value = currencies.value.filter((x) => x !== currency);
 };
 
-const updateActiveCurrency = async (currency: TCurrency) => {
+const updateActiveCurrency = async (currency: TCurrency): Promise<void> => {
   if (activeCurrency.value === currency) {
     return;
   }
@@ -85,48 +92,48 @@ const updateActiveCurrency = async (currency: TCurrency) => {
   await fetchCurrencies();
 };
 
+const updateFormula = (updatedFormula: TFormula): void => {
+  formula.value = updatedFormula;
+};
+
 onMounted(() => {
   prepare();
 });
 </script>
 
 <template>
-  <div class="navbar mb-5">
-    <div class="navbar-start">
-      <Links></Links>
-    </div>
-  </div>
+  <Navbar class="px-3 sm:px-6"></Navbar>
+  <Work :update-formula="updateFormula" class="mt-5 mb-3"></Work>
   <CurrencySelector
     class="mb-5"
     :add-new-currency="addNewCurrency"
   ></CurrencySelector>
-  <table
-    class="table table-xs w-full max-w-2xl border-spacing-y-3 border-separate mx-auto"
-  >
-    <thead>
-      <tr>
-        <th>Currency</th>
-        <th>Hourly</th>
-        <th>Daily</th>
-        <th>Weekly</th>
-        <th>Monthly</th>
-        <th>Annually</th>
-      </tr>
-    </thead>
-    <tbody>
-      <CurrencyRow
-        v-if="activeCurrency"
-        :currency="currency"
-        :currency-rate="
-          exchangeRates?.rates.find((x) => x.name === currency.name)?.amount
-        "
-        :update-active-currency="updateActiveCurrency"
-        :remove-currency="removeCurrency"
-        :key="currency.abbr"
-        v-for="currency in currencies"
-      ></CurrencyRow>
-    </tbody>
-  </table>
+  <!-- Table Start -->
+  <div class="overflow-auto">
+    <table
+      class="table table-xs max-w-xs md:max-w-2xl lg:max-w-3xl border-spacing-y-3 border-separate mx-auto"
+    >
+      <thead>
+        <CurrencyHeader></CurrencyHeader>
+      </thead>
+      <tbody>
+        <CurrencyRow
+          v-if="activeCurrency && formula"
+          :formula="formula"
+          :currency="currency"
+          :currency-rate="
+            exchangeRates?.rates.find((x) => x.name === currency.name)?.amount
+          "
+          :update-active-currency="updateActiveCurrency"
+          :remove-currency="removeCurrency"
+          :key="currency.abbr"
+          v-for="currency in currencies"
+        ></CurrencyRow>
+      </tbody>
+    </table>
+  </div>
+
+  <!-- Table End -->
 </template>
 
 <style scoped></style>
