725a71e920b75247588112fc8a1eae7e086b6b2d
Üsame <usame.kilicbay@gmail.com>
2025-02-03 22:24:33 +0300

    update!: major changes and not finished

--- src/components/common/InputInstantNumber.vue
+++ src/components/common/InputInstantNumber.vue
@@ -1,14 +1,15 @@
 <script lang="ts" setup>
-import { computed, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { enforceMinMaxWithInputEvent } from "../../helpers/valueEnforcer";
 import { TNumberInputState } from "../../common/type";
+import { isValidNumber } from "../../helpers/eventNumberParser";
 
 const props = defineProps<{
   id: string;
+  originalValue: number;
   min: number;
   max: number;
   allowEmpty?: boolean;
-  inputState: TNumberInputState;
   validClass: string;
   invalidClass: string;
 }>();
@@ -18,17 +19,30 @@ const emit = defineEmits<{
   (event: "update:inputState", value: TNumberInputState): void;
 }>();
 
+// TODO: Remove
 // const value = ref<number>(1);
-const tempValue = ref<number>(1);
+const tempValue = ref<number>(0);
 
 const inputState = computed<TNumberInputState>(() => {
-  // Less than minimum limit
-  if (tempValue.value < props.min && props.max > 0) return "small";
-  // More than maximum limit
-  if (tempValue.value > props.max) return "big";
-  // Within the valid range
-  if (tempValue.value > props.min && tempValue.value < props.max)
-    return "valid";
+  // TODO: Remove
+  if (props.id === "annual-leave") {
+    console.log(tempValue.value, "annual");
+  }
+  // Valid number input
+  if (isValidNumber(tempValue.value)) {
+    // Less than minimum limit
+    if (tempValue.value < props.min) {
+      // Empty but allowed (valid)
+      if (props.allowEmpty) return "valid";
+      return "small";
+    }
+    // More than maximum limit
+    if (tempValue.value > props.max) return "big";
+    // Within the valid range
+    if (tempValue.value >= props.min && tempValue.value <= props.max)
+      return "valid";
+  }
+
   // Empty but allowed (valid)
   if (props.allowEmpty) return "valid";
   // Empty (invalid)
@@ -36,6 +50,7 @@ const inputState = computed<TNumberInputState>(() => {
 });
 
 //#region HANDLE
+// TODO: Remove
 // Handles the input for annual leave and updates the duration if the input is valid.
 // const handleOnInput = () => {
 // inputState.value === "valid" && (value.value = tempValue.value);
@@ -51,17 +66,29 @@ const handleOnChange = ($event: Event) => {
 };
 //#endregion
 
+onMounted(() => (tempValue.value = props.originalValue));
+
 watch(tempValue, () => {
-  emit("update:modelValue", tempValue.value);
+  if (inputState.value === "valid") {
+    emit("update:modelValue", tempValue.value);
+  }
 });
 
-watch(inputState, () => emit("update:inputState", inputState.value));
+watch(inputState, () => {
+  emit("update:inputState", inputState.value);
+});
+
+watch(
+  () => props.originalValue,
+  () => (tempValue.value = props.originalValue)
+);
 </script>
 
 <template>
   <input
     :id="props.id"
-    v-model="tempValue"
+    type="number"
+    v-model.number="tempValue"
     @change="handleOnChange($event)"
     :class="{
       [props.validClass]: inputState === 'valid',
