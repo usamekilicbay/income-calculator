d1125e1b3403724ab7ded1e4af2a7007d10d5ca9
Üsame <usame.kilicbay@gmail.com>
2025-02-04 09:52:55 +0300

    add: implement vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { Analytics } from "@vercel/analytics/vue";
 import CalculatorMain from "./components/CalculatorMain.vue";
 </script>
 
 <template>
+  <Analytics />
   <CalculatorMain />
 </template>
